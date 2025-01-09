283973a79fbc4573916897d42de2cb10e2370e9e
Matt Bannert <bunny@hey.com>
2025-01-10 00:20:10 +0100

    updated collection, avatar removed

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,11 +9,8 @@ const teamCollection = defineCollection({
   schema: z.object({
     draft: z.boolean(),
     name: z.string(),
+    shortname: z.string(),
     title: z.string(),
-    avatar: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
     publishDate: z.string().transform(str => new Date(str)),
   }),
 });
